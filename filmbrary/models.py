c75d31a37c3ad21e118d9a5c5bbcb0c2c61d3f88
starpallace <103674746+starpallace@users.noreply.github.com>
2022-08-24 00:51:00 +0300

    added taggable manager

--- filmbrary/models.py
+++ filmbrary/models.py
@@ -2,7 +2,21 @@ from django.db import models
 
 
 class Film(models.Model):
-    film_name = models.CharField(max_length=200)
-    film_year = models.PositiveSmallIntegerField
-    description = models.TextField
-    poster = models.ImageField
+     
+    slug= models.SlugField
+    name = models.CharField(max_length=200)
+    year = models.PositiveSmallIntegerField
+    desc = models.TextField
+    poster = models.ImageField(default='movie.jpg', upload_to='posters')
+    screens = models.ImageField(blank=True)
+    taggs_plus = TaggableManager()
+    taggs_minus = TaggableManager()
+
+    def __str__(self):
+        return self.name
+
+    def shorts(self):
+        return self.desc[:160] + '...'
+
+
+        
--- filmbrary/settings.py
+++ filmbrary/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'films.apps.FilmsConfig',
+    'taggit',
 ]
 
 MIDDLEWARE = [
