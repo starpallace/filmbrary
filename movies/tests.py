4185dfd0b719507f604c37b9b93a46b4de6470a0
starpallace <103674746+starpallace@users.noreply.github.com>
2023-04-15 12:48:29 +0300

    changes to test settings

--- movies/tests.py
+++
@@ -1,6 +0,0 @@
-from django.test import TestCase
-
-class URLTests(TestCase):
-    def test_homepage(self):
-        responce = self.client.get('/')
-        self.assertEqual(responce.status_code,200)
---
+++ tests/test_01.py
@@ -0,0 +1,7 @@
+import pytest
+
+def test_stupid():
+    assert 1==1
+
+def test_failer():
+    assert 'planet'=='planet'
